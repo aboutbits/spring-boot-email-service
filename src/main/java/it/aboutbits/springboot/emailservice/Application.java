46e6e2ba506da09e32731cf55f75d7d1b7125062
Andreas Hufler <andreas.hufler@aboutbits.it>
2023-07-11 12:04:05 +0200

    project init

---
+++ src/main/java/it/aboutbits/springboot/emailservice/Application.java
@@ -0,0 +1,13 @@
+package it.aboutbits.springboot.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/aboutbits/springboot/emailservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aboutbits.springboot.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
